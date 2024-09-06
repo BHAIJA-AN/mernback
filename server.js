9c8a7ec7073d9f460a29daaae3fa60db3b934c53
Ahashan Mansuri <b210004@nitsikkim.ac.in>
2024-09-06 23:12:38 +0530

    dotenv added

--- server.js
+++ server.js
@@ -8,21 +8,10 @@ const errorMiddleware = require("./middlewares/error-middleware");
 const FETCH = process.env.ALLOW_REQUEST;
 const app = express();
 
-// Split the FETCH variable into an array of origins if it contains multiple URLs
-const allowedOrigins = FETCH.split(',');
-
-// CORS configuration using the FETCH variable
 const corsOption = {
-  origin: function (origin, callback) {
-    // If origin is in the allowedOrigins array or it's undefined (e.g., for non-browser requests), allow it
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: "GET, PUT, POST, DELETE, PATCH, HEAD", // Allowed HTTP methods
-  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  origin: FETCH,
+  methods: "GET, PUT, POST, DELETE, PATCH, HEAD",
+  credentials: true,
 };
 // Apply the CORS middleware with the specified options
 app.use(cors(corsOption));
