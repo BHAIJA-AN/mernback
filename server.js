3b43080bb078d13d28598d01825f76fbb2d9ef89
Ahashan Mansuri <b210004@nitsikkim.ac.in>
2024-09-06 20:15:14 +0530

    rresolve dotenv

--- server.js
+++ server.js
@@ -5,15 +5,27 @@ const PORT = process.env.PORT;
 const connectDB = require("./utils/db");
 const contact_router= require("./router/contact-router");
 const errorMiddleware = require("./middlewares/error-middleware");
-
+const FETCH = process.env.ALLOW_REQUEST;
 const app = express();
 
+// Split the FETCH variable into an array of origins if it contains multiple URLs
+const allowedOrigins = FETCH.split(',');
+
+// CORS configuration using the FETCH variable
 const corsOption = {
-  origin: "http://localhost:5173",
-  methods: "GET, PUT, POST, DELETE, PATCH, HEAD",
-  credentials: true,
+  origin: function (origin, callback) {
+    // If origin is in the allowedOrigins array or it's undefined (e.g., for non-browser requests), allow it
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: "GET, PUT, POST, DELETE, PATCH, HEAD", // Allowed HTTP methods
+  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
 };
 
+// Apply the CORS middleware with the specified options
 app.use(cors(corsOption));
 app.use(express.json());
 
